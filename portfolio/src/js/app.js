fc0e2022843c0b943681534a577e3a3ecf6a405b
hist-site <63604237+hist-site@users.noreply.github.com>
2021-10-18 01:05:07 +0300

    in com

--- portfolio/src/js/app.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".toggle").addEventListener("click", () => {
-    document.querySelector(".toggle").classList.toggle('active');
-    document.querySelector(".mobile-navigation").classList.toggle('active');
-});
